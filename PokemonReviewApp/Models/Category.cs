60557d30d48dce82cfd2bad6c38bdd9a3d3f2a31
kaushikchitipothu <kaushikchitipothu5@gmail.com>
2024-08-16 14:01:00 -0400

    Add project files.

---
+++ PokemonReviewApp/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<PokemonOwners> pokemonOwners { get; set; }
+    }
+}
---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public string name {  get; set; }
+        public int id { get; set; }
+        public string description { get; set; }
+        public ICollection<Review> reviews { get; set; }
+        public ICollection<PokemonOwners> pokemonOwners { get; set; }
+        public ICollection<PokemonCategories> pokemonCategories { get; set; }
+
+    }
+}
